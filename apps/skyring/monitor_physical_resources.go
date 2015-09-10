63778c9b0a5c8c81c8fd600fb0fbf38f4d446a6b
Shubhendu Tripathi <shtripat@redhat.com>
2015-09-10 15:39:30 +0530

    skyring: Added host management APIs
    Added below host management APIs
        - GET "/hosts" to list the managed hosts
        - GET "/hosts/{host-name}" to list the specific managed host
        - PUT "/hosts" to add a host
        - GET "/hosts/{host-name}/cpu_utilization"
        - GET "/hosts/{host-name}/memory_utilization"
    
    Change-Id: I7a1c075465a6419c78585a9ac6f0b7bdd3a35af4
    Signed-off-by: Shubhendu Tripathi <shtripat@redhat.com>

---
+++ apps/skyring/monitor_physical_resources.go
@@ -0,0 +1,109 @@
+/*Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package skyring
+
+import (
+    "encoding/json"
+    "log"
+    "fmt"
+    "net/url"
+    "net/http"
+    "github.com/gorilla/mux"
+
+    influxdb "github.com/influxdb/influxdb/client"
+)
+
+const (
+    DBHost = "influxdb"
+    DBPort = 8086
+    DBName = "collectd"
+    DBUSer = "admin"
+    DBPasswd = "admin"
+)
+
+func queryDB(con *influxdb.Client, cmd string) (res []influxdb.Result, err error) {
+    q := influxdb.Query {
+        Command: cmd,
+        Database: DBName,
+    }
+
+    if response, err := con.Query(q); err == nil {
+        if response.Error() != nil {
+            return res, response.Error()
+        }
+        res = response.Results
+    }
+    return
+}
+
+func CpuUtilizationHandler(w http.ResponseWriter, r *http.Request) {
+    u, err := url.Parse(fmt.Sprintf("http://%s:%d", DBHost, DBPort))
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    client, err := influxdb.NewClient(influxdb.Config {
+        URL: *u,
+        Username: DBUSer,
+        Password: DBPasswd,
+    })
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    vars := mux.Vars(r)
+    host_name := vars["host-name"]
+
+    query_cmd := fmt.Sprintf("SELECT * FROM /(%s.cpu).*/", host_name)
+    res, err := queryDB(client, query_cmd)
+    if err == nil {
+        json.NewEncoder(w).Encode(res[0].Series)
+    } else {
+        w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+        w.WriteHeader(422)
+        if err := json.NewEncoder(w).Encode(err); err != nil {
+            panic(err)
+        }
+    }
+}
+
+func MemoryUtilizationHandler(w http.ResponseWriter, r *http.Request) {
+    u, err := url.Parse(fmt.Sprintf("http://%s:%d", DBHost, DBPort))
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    client, err := influxdb.NewClient(influxdb.Config {
+        URL: *u,
+        Username: DBUSer,
+        Password: DBPasswd,
+    })
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    vars := mux.Vars(r)
+    host_name := vars["host-name"]
+
+    query_cmd := fmt.Sprintf("SELECT * FROM /(%s.memory).*/", host_name)
+    res, err := queryDB(client, query_cmd)
+    if err == nil {
+        json.NewEncoder(w).Encode(res[0].Series)
+    } else {
+        w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+        w.WriteHeader(422)
+        if err := json.NewEncoder(w).Encode(err); err != nil {
+            panic(err)
+        }
+    }
+}
