92e7e41b57fa796a2b0e5fc25c0ae4c9e05ed569
Bala.FA <barumuga@redhat.com>
2015-09-26 20:12:36 +0530

    tools: add task management
    Task management provides a framework to run function as task.  Below
    is an example code
    
    import "github.com/skyrings/skyring/tools/task"
    
    // function awares of task.Task
    sleeper := func(t *task.Task) {
    	t.UpdateStatus("started the task: %s", t.ID)
    	t.UpdateStatus("going to sleep 3s")
    	time.Sleep(time.Second * 3)
    	t.UpdateStatus("i am done")
    	t.Done()
    }
    
    // new task manager
    m := task.NewManager()
    // run the function as task
    id, _ := m.Run("sleeper", sleeper)
    // get status after sometimes
    m.GetStatus(*id)
    // is the task completed?
    m.IsDone(*id)
    // remove the task upon completion
    m.Remove(*id)
    
    Change-Id: Ib8644ad62103a37a4e799846249805484e911b5c
    Signed-off-by: Bala.FA <barumuga@redhat.com>

---
+++ tools/task/manager.go
@@ -0,0 +1,76 @@
+// Copyright 2015 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package task
+
+import (
+	"errors"
+	"fmt"
+	"github.com/skyrings/skyring/tools/uuid"
+)
+
+type Manager struct {
+	tasks map[uuid.UUID]*Task
+}
+
+func (manager *Manager) Run(name string, f func(t *Task)) (id *uuid.UUID, err error) {
+	if id, err = uuid.New(); err == nil {
+		task := Task{
+			ID: *id,
+			Name: name,
+			DoneCh: make(chan bool, 1),
+			StatusList: []Status{},
+			StopCh: make(chan bool, 1),
+			Func: f,
+		}
+		task.Run()
+		manager.tasks[*id] = &task
+	}
+	return
+}
+
+func (manager *Manager) IsDone(id uuid.UUID) (b bool, err error) {
+	if task, ok := manager.tasks[id]; ok {
+		b = task.IsDone()
+	} else {
+		err = errors.New(fmt.Sprintf("task id %s not found", id))
+	}
+	return
+}
+
+func (manager *Manager) GetStatus(id uuid.UUID) (status []Status, err error) {
+	if task, ok := manager.tasks[id]; ok {
+		status = task.GetStatus()
+	} else {
+		err = errors.New(fmt.Sprintf("task id %s not found", id))
+	}
+	return
+}
+
+func (manager *Manager) Remove(id uuid.UUID) {
+	delete(manager.tasks, id)
+}
+
+func (manager *Manager) List() []uuid.UUID {
+	ids := make([]uuid.UUID, 0, len(manager.tasks))
+	for k := range manager.tasks {
+		ids = append(ids, k)
+	}
+
+	return ids
+}
+
+func NewManager() Manager {
+	return Manager{make(map[uuid.UUID]*Task)}
+}
