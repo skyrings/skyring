4ce238efd152f2bc1cf04bf2dd125ca8c7bd5f12
Shubhendu Tripathi <shtripat@redhat.com>
2015-11-03 13:51:04 +0530

    rest: apis to create and fetch storage entities
    Introduced the below apis-
      - POST /api/<ver>/clusters/{cluster-id}/storages
        to create a storage entity. Request payload -
          {
            "name": "<storage name>",
            "type": "<type>",
            "tags": ["value1", "value2", ...],
            "size": <int value>,
            "options": {"provider": "specific", "key": "value", ...}
          }
      - GET /api/<ver>/clusters/{cluster-id}/storages
        to get the list of storages created for a cluster
      - GET /api/<ver>/clusters/{cluster-id}/storages/{storage-id}
        to get a specific storage entity
    
    Change-Id: I763634ccc53562053b3f76458ebd9d3fc423a016
    Signed-off-by: Shubhendu Tripathi <shtripat@redhat.com>

---
+++ apps/skyring/storage.go
@@ -0,0 +1,133 @@
+/*Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package skyring
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/skyrings/skyring/conf"
+	"github.com/skyrings/skyring/db"
+	"github.com/skyrings/skyring/models"
+	"github.com/skyrings/skyring/tools/logger"
+	"github.com/skyrings/skyring/tools/uuid"
+	"github.com/skyrings/skyring/utils"
+	"gopkg.in/mgo.v2/bson"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+var (
+	storage_post_functions = map[string]string{
+		"create": "CreateStorage",
+		"expand": "ExpandStorage",
+	}
+
+	STORAGE_STATUS_UP   = "up"
+	STORAGE_STATUS_DOWN = "own"
+)
+
+func (a *App) POST_Storages(w http.ResponseWriter, r *http.Request) {
+	var request models.AddStorageRequest
+
+	// Unmarshal the request body
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, models.REQUEST_SIZE_LIMIT))
+	if err != nil {
+		logger.Get().Error("Error parsing the request: %v", err)
+		util.HttpResponse(w, http.StatusBadRequest, fmt.Sprintf("Unable to parse the request: %v", err))
+		return
+	}
+	if err := json.Unmarshal(body, &request); err != nil {
+		util.HttpResponse(w, http.StatusBadRequest, fmt.Sprintf("Unable to unmarshal request: %v", err))
+		return
+	}
+
+	// Check if storage entity already added
+	if storage, _ := storage_exists("name", request.Name); storage != nil {
+		util.HttpResponse(w, http.StatusMethodNotAllowed, "Storage entity already added")
+		return
+	}
+
+	vars := mux.Vars(r)
+	cluster_id_str := vars["cluster-id"]
+	cluster_id, _ := uuid.Parse(cluster_id_str)
+	var result models.RpcResponse
+	// Get the specific provider and invoke the method
+	provider := a.getProviderFromClusterId(*cluster_id)
+	err = provider.Client.Call(fmt.Sprintf("%s.%s",
+		provider.Name, storage_post_functions["create"]),
+		models.RpcRequest{RpcRequestVars: mux.Vars(r), RpcRequestData: body},
+		&result)
+	if err != nil || result.Status.StatusCode != http.StatusOK {
+		util.HttpResponse(w, http.StatusInternalServerError, fmt.Sprintf("Error while storage creation %v", err))
+		return
+	}
+}
+
+func storage_exists(key string, value string) (*models.Storage, error) {
+	sessionCopy := db.GetDatastore().Copy()
+	defer sessionCopy.Close()
+
+	collection := sessionCopy.DB(conf.SystemConfig.DBConfig.Database).C(models.COLL_NAME_STORAGE)
+	var storage models.Storage
+	if err := collection.Find(bson.M{key: value}).One(&storage); err != nil {
+		return nil, err
+	} else {
+		return &storage, nil
+	}
+}
+
+func (a *App) GET_Storages(w http.ResponseWriter, r *http.Request) {
+	sessionCopy := db.GetDatastore().Copy()
+	defer sessionCopy.Close()
+
+	collection := sessionCopy.DB(conf.SystemConfig.DBConfig.Database).C(models.COLL_NAME_STORAGE)
+	var storages models.Storages
+	if err := collection.Find(nil).All(&storages); err != nil {
+		util.HttpResponse(w, http.StatusInternalServerError, err.Error())
+		logger.Get().Error("Error getting the storage list: %v", err)
+		return
+	}
+	if len(storages) == 0 {
+		json.NewEncoder(w).Encode(models.Storages{})
+	} else {
+		json.NewEncoder(w).Encode(storages)
+	}
+}
+
+func (a *App) GET_Storage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	cluster_id_str := vars["cluster-id"]
+	cluster_id, _ := uuid.Parse(cluster_id_str)
+	storage_id_str := vars["storage-id"]
+	storage_id, _ := uuid.Parse(storage_id_str)
+
+	sessionCopy := db.GetDatastore().Copy()
+	defer sessionCopy.Close()
+
+	collection := sessionCopy.DB(conf.SystemConfig.DBConfig.Database).C(models.COLL_NAME_STORAGE)
+	var storage models.Storage
+	if err := collection.Find(bson.M{"clusterid": *cluster_id, "storageid": *storage_id}).One(&storage); err != nil {
+		util.HttpResponse(w, http.StatusInternalServerError, err.Error())
+		logger.Get().Error("Error getting the storage: %v", err)
+		return
+	}
+	if storage.Name == "" {
+		util.HttpResponse(w, http.StatusBadRequest, "Storage not found")
+		logger.Get().Error("Storage not found: %v", err)
+		return
+	} else {
+		json.NewEncoder(w).Encode(storage)
+	}
+}
