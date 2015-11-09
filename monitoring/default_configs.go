6da0a1c6d2a9a98cbaae3f9b84163f75806e0c2f
anmolbabu <anmolbudugutta@gmail.com>
2015-11-09 11:33:33 +0530

    monitoring : Collectd-Salt formulae + Configuration framework
    This patch adds
    1. Formulae to configure collectd plugins for
       monitoring physical resources like:
      (i) CPU Utilization
     (ii) Memory Utilization
    (iii) Network Utilization
     (iv) inode Usage
      (v) iops
     (vi) Swap Utilization
    2. Adds framework for collectd configuration from skyring
    
    Change-Id: I0c5186ba0ae9cbf421ce404e57f06584a3e88793
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

---
+++ monitoring/default_configs.go
@@ -0,0 +1,27 @@
+package monitoring
+
+func GetDefaultThresholdValues() (plugins []Plugin) {
+	return []Plugin{
+		{
+			Name:   "df",
+			Enable: true,
+			Configs: []PluginConfig{
+				{Category: "threshold", Type: "critical", Value: "70"},
+			},
+		},
+		{
+			Name:   "memory",
+			Enable: true,
+			Configs: []PluginConfig{
+				{Category: "threshold", Type: "critical", Value: "70"},
+			},
+		},
+		{
+			Name:   "cpu",
+			Enable: true,
+			Configs: []PluginConfig{
+				{Category: "threshold", Type: "critical", Value: "70"},
+			},
+		},
+	}
+}
