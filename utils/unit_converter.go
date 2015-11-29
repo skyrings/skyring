c41ea4521d2fb91374722ea819f3381099a29ca4
anmolbabu <anmolbudugutta@gmail.com>
2015-11-29 16:46:15 +0530

    utils : Add UnitConverter
    This patch adds UnitConverter which is a generic
    to handle conversion from any unit to any other unit
    in a given array of units.
    
    Change-Id: Ie3a0a7d91b849c1382a3e74d2f92abfa5656bba4
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

---
+++ utils/unit_converter.go
@@ -0,0 +1,100 @@
+package utils
+
+type Unit struct {
+	Name string
+	NoOfPrevUnitEqui int
+	Order int
+}
+
+type Units []Unit
+
+func (slice Units) Len() int {
+    return len(slice)
+}
+
+func (slice Units) Less(i, j int) bool {
+    return slice[i].Order < slice[j].Order;
+}
+
+func (slice Units) Swap(i, j int) {
+    slice[i], slice[j] = slice[j], slice[i]
+}
+
+func GetSizeUnits() (Units) {
+	return Units {
+		{
+			Name: "Bytes",
+			NoOfPrevUnitEqui: 1,
+			Order: 1,
+		},
+		{
+			Name: "KibiBytes",
+			NoOfPrevUnitEqui: 1024,
+			Order: 2,
+		},
+		{
+			Name: "MibiBytes",
+			NoOfPrevUnitEqui: 1024,
+			Order: 3,
+		},
+		{
+			Name: "GibiBytes",
+			NoOfPrevUnitEqui: 1024,
+			Order: 4,
+		},
+		{
+			Name: "TibiBytes",
+			NoOfPrevUnitEqui: 1024,
+			Order: 5,
+		},
+	}
+}
+
+func MakeUnits(orderedUnitNames []string, commonScale int) (units Units){
+	for index, unitName := range orderedUnitNames {
+		noOfPrevUnitEqui := commonScale
+		if index == 0 {
+			noOfPrevUnitEqui = 1
+		}
+		units = append(units, Unit{Name : unitName, NoOfPrevUnitEqui : noOfPrevUnitEqui, Order: index + 1 })
+	}
+	return units
+}
+
+var SizeUnits Units = MakeUnits([]string{"Bytes", "KibiBytes", "MibiBytes", "GibiBytes", "TibiBytes"}, 1024)
+
+func (fromUnit Unit) Convert(fromValue float64, toUnit Unit, unitSet Units) (size float64) {
+	size = fromValue
+	loopOk := fromUnit.Order != toUnit.Order
+	index := fromUnit.Order
+	for ;loopOk; {
+		if fromUnit.Order < toUnit.Order {
+			size = size/(float64(unitSet[index].NoOfPrevUnitEqui))
+			index++
+		} else {
+			index--
+			size = size * (float64(unitSet[index].NoOfPrevUnitEqui))
+		}
+		if toUnit.Order == index {
+			loopOk = false
+		}
+	}
+	return size
+}
+
+func (fromUnit Unit) AutoConvert(fromValue float64, unitSet Units) (size float64, unit Unit) {
+	size = fromValue
+	index := fromUnit.Order - 1
+	currentUnit := fromUnit
+	if index != len(unitSet) - 1 {
+		for ;; {
+			index = index + 1
+			size = currentUnit.Convert(size, unitSet[index], unitSet)
+			currentUnit = unitSet[index]
+			if index == len(unitSet) - 1  || size < float64(unitSet[index+1].NoOfPrevUnitEqui) {
+				return size, unitSet[index]
+			}
+		}
+	}
+	return size, unitSet[index]
+}
