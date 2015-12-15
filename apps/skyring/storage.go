7de7c1fa53bc7649988b14abd5104399af712672
Shubhendu Tripathi <shtripat@redhat.com>
2015-12-16 00:16:57 +0530

    skyring: Time out after 5 mins if pool creation not responding
    Change-Id: Ib09084f268d0fc153adfb84c211106384acd7d13
    Signed-off-by: Shubhendu Tripathi <shtripat@redhat.com>

--- apps/skyring/storage.go
+++ apps/skyring/storage.go
@@ -114,7 +114,8 @@ func (a *App) POST_Storages(w http.ResponseWriter, r *http.Request) {
 			}
 
 			// Check for provider task to complete and update the parent task
-			for {
+			done := false
+			for count := 0; count < 150; count++ {
 				time.Sleep(2 * time.Second)
 				sessionCopy := db.GetDatastore().Copy()
 				defer sessionCopy.Close()
@@ -132,8 +133,15 @@ func (a *App) POST_Storages(w http.ResponseWriter, r *http.Request) {
 						t.UpdateStatus("Failed")
 						t.Done(models.TASK_STATUS_FAILURE)
 					}
+					done = true
 					break
 				}
+			}
+			if !done {
+				util.FailTask(
+					"Sub task timed out",
+					errors.New("Could not get sub task status after 5 minutes"),
+					t)
 			}
 		}
 	}
