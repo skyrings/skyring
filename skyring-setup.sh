89ff40940e1cc2f3c899e0d0934f449ef941d3ec
Timothy Asir J <asirtim@yahoo.com>
2015-12-22 20:11:07 +0530

    rpm: enhance rpm build scripts
    1) updated Mongodb and Mongodb-server package dependency details
    based on os type.
    2) removed python-paramiko dependency
    3) removed ceph package dependency. skyring work with Ceph via
    REST API(Calamari) which does not required 'ceph' package
    dependency
    4) Added salt version 2015.5.5 package dependency
    5) Removed 'set -e' from skyring-setup.sh script to avoid
    script to stop suddenly for minor issues
    6) Mongodb enable, start, script execution procedures updated
    based on os type.
    
    Change-Id: I4d6f98a8c061cf3daa52cf59867c26be0bf6ce85
    Signed-off-by: Timothy Asir J <asirtim@yahoo.com>

--- skyring-setup.sh
+++ skyring-setup.sh
@@ -3,8 +3,6 @@
 # This is a single script which will install required packages,
 # Configure and setting up the system to use skyring
 
-set -e
-
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
@@ -40,8 +38,13 @@ systemctl enable influxdb
 systemctl start influxdb
 
 # Enable and start MongoDB
-systemctl enable mongod
-systemctl start mongod
+if ! grep -q '^Red Hat Enterprise Linux Server release' /etc/redhat-release ; then
+    systemctl enable mongod
+    systemctl start mongod
+else
+    systemctl enable rh-mongodb26-mongod.service
+    systemctl start rh-mongodb26-mongod.service
+fi
 
 # Need to wait for 3 to 5 sec for the services to comes up
 sleep 5
@@ -55,6 +58,7 @@ info "Creating time series database"
 
 info "Creating skyring database"
 # Configuring MongoDB
+if ! grep -q '^Red Hat Enterprise Linux Server release' /etc/redhat-release; then
 mongo <<EOF
 use skyring
 db.leads.findOne()
@@ -62,6 +66,15 @@ show collections
 db.createUser( { "user" : "admin", "pwd": "admin", "roles" : ["readWrite", "dbAdmin", "userAdmin"] })
 show users
 EOF
+else
+scl enable rh-mongodb26 'mongo' <<EOF
+use skyring
+db.leads.findOne()
+show collections
+db.createUser( { "user" : "admin", "pwd": "admin", "roles" : ["readWrite", "dbAdmin", "userAdmin"] })
+show users
+EOF
+fi
 
 info "\n\n\n-------------------------------------------------------"
 info "Now the host setup is ready!"
