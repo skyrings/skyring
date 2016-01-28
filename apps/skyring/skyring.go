7316ba144b1c2602af7defe7292c812432ff08aa
nthomas-redhat <nishusemail@gmail.com>
2016-01-28 18:12:19 +0530

    skyring: Fixed initialization related issue
    Fixed skyring crashes when mongodb auth fails
    
    Bug: https://bugzilla.redhat.com/show_bug.cgi?id=1299009
    Change-Id: I8ab748ca55d5adfe553e1aae3b3ed2739f61e38a
    Signed-off-by: nthomas-redhat <nishusemail@gmail.com>

--- apps/skyring/skyring.go
+++ apps/skyring/skyring.go
@@ -14,6 +14,7 @@ package skyring
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
@@ -77,21 +78,11 @@ func NewApp(configDir string, binDir string) *App {
 	app.providers = make(map[string]Provider)
 	app.routes = make(map[string]conf.Route)
 
-	//Load providers and routes
-	//Load all the files present in the config path
-	app.StartProviders(configDir, binDir)
-
-	//Check if atleast one provider is initialized successfully
-	//otherwise panic
-	/*if len(app.providers) < 1 {
-		panic(fmt.Sprintf("None of the providers are initialized successfully"))
-	}
-	logger.Get().Info("Loaded URLs:", app.routes)*/
 	application = app
 	return app
 }
 
-func (a *App) StartProviders(configDir string, binDir string) {
+func (a *App) StartProviders(configDir string, binDir string) error {
 
 	providerBinaryPath := path.Join(binDir, ProviderBinaryDir)
 
@@ -116,17 +107,20 @@ func (a *App) StartProviders(configDir string, binDir string) {
 		}
 		//If duplicate routes are detected, dont proceed loading this provider
 		if found {
+			logger.Get().Error("Duplicate routes detected, skipping the provider", config)
 			continue
 		}
 		//Start the provider if configured
 		if config.Provider != (conf.ProviderConfig{}) {
 
 			if config.Provider.Name == "" {
+				logger.Get().Error("Provider Name Empty, skipping the provider", config)
 				continue
 			}
 			//check whether the plugin is initialized already
 			if _, ok := a.providers[config.Provider.Name]; ok {
 				//Provider already initialized
+				logger.Get().Info("Provider already initialized, skipping the provider", config)
 				continue
 			}
 			if config.Provider.ProviderBinary == "" {
@@ -152,6 +146,10 @@ func (a *App) StartProviders(configDir string, binDir string) {
 			a.providers[config.Provider.Name] = Provider{Name: config.Provider.Name, Client: client}
 		}
 	}
+	if len(a.providers) < 1 {
+		return errors.New("None of the providers are initialized successfully")
+	}
+	return nil
 }
 
 func (a *App) SetRoutes(router *mux.Router) error {
