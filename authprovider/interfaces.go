cf1ff586be9ea4fde0b6e589efd388ce3a8d05e3
root <root@dhcp35-209.lab.eng.blr.redhat.com>
2016-02-13 20:17:10 +0530

    Add provision to add or update or get ldap configuration
    Rest api for get ldap configuration:
    GET: http://localhost:8080/api/v1/ldap
    
    Rest api for add or update ldap configuration
    POST: http://localhost:8080/api/v1/ldap
    {"ldapserver": "ip",
    "port": 389,
    "base": "OU=Users,DC=example,DC=com",
    "domainadmin": "<any user in the group or admin",
    "password": "<pwd> this will be encrypted",
    "uid": "cn",
    "firstname": "cn",
    "lastname": "sn",
    "mail": "mail"}
    
    Change-Id: I71a5bbc8690e953a6dbbc091030c7492723fc270
    Signed-off-by: Timothy Asir Jeyasing <tjeyasin@redhat.com>
    Signed-off-by: Timothy Asir <tjeyasin@redhat.com>

--- authprovider/interfaces.go
+++ authprovider/interfaces.go
@@ -38,4 +38,6 @@ type AuthInterface interface {
 	ListUsers() ([]models.User, error)
 	DeleteUser(username string) error
 	ListExternalUsers(searchstr string, page, no int) (models.ExternalUsers, error)
+	SetDirectory(directory models.Directory) error
+	GetDirectory() (models.Directory, error)
 }
