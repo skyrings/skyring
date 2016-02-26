d584fad089c38ed754797326d43ad533964b578b
DhivyaSiva <dhivyasivaprakasam9@gmail.com>
2016-02-26 13:12:56 +0530

    Skyring: Added an api to test the mail notifier
    Added the below API
      - POST /api/<ver>/testmailnotifier/{recipient}
        It sends a sample mail to the recipient passed in request
    query parameter.
    
    Change-Id: I0b43d911852d1203ecaae3c4dc2ad83ce891b302
    Signed-off-by: DhivyaSiva <dhivyasivaprakasam9@gmail.com>

--- apps/skyring/notifier.go
+++ apps/skyring/notifier.go
@@ -17,6 +17,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/skyrings/skyring-common/dbprovider/mongodb"
 	"github.com/skyrings/skyring-common/models"
 	mail_notifier "github.com/skyrings/skyring-common/notifier"
@@ -43,28 +44,7 @@ func (a *App) GetMailNotifier(rw http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func (a *App) AddMailNotifier(rw http.ResponseWriter, req *http.Request) {
-	if req.Method == "POST" {
-		_, err := GetDbProvider().MailNotifierInterface().MailNotifier()
-		if err != mongodb.ErrMissingNotifier {
-			HandleHttpError(rw, errors.New("Mail Notifier Record Already exists"))
-			return
-		}
-	}
-
-	body, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		logger.Get().Error("Error parsing http request body:%s", err)
-		HandleHttpError(rw, err)
-		return
-	}
-	var m map[string]interface{}
-
-	if err = json.Unmarshal(body, &m); err != nil {
-		logger.Get().Error("Unable to Unmarshall the data:%s", err)
-		HandleHttpError(rw, err)
-		return
-	}
+func GetDetails(m map[string]interface{}) (models.MailNotifier, error) {
 
 	var notifier models.MailNotifier
 	notifier.SkipVerify = true
@@ -87,9 +67,48 @@ func (a *App) AddMailNotifier(rw http.ResponseWriter, req *http.Request) {
 	if val, ok := m["encryption"]; ok {
 		notifier.Encryption = val.(string)
 	}
+	if val, ok := m["mailnotification"]; ok {
+		notifier.MailNotification = val.(bool)
+	} else {
+		return notifier, err
+	}
+	if val, ok := m["subprefix"]; ok {
+		notifier.SubPrefix = val.(string)
+	}
+	return notifier, nil
+}
+
+func (a *App) AddMailNotifier(rw http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		_, err := GetDbProvider().MailNotifierInterface().MailNotifier()
+		if err != mongodb.ErrMissingNotifier {
+			HandleHttpError(rw, errors.New("Mail Notifier Record Already exists"))
+			return
+		}
+	}
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		logger.Get().Error("Error parsing http request body:%s", err)
+		HandleHttpError(rw, err)
+		return
+	}
+	var m map[string]interface{}
+
+	if err = json.Unmarshal(body, &m); err != nil {
+		logger.Get().Error("Unable to Unmarshall the data:%s", err)
+		HandleHttpError(rw, err)
+		return
+	}
+	notifier, err := GetDetails(m)
+	if err != nil {
+		logger.Get().Error("Insufficient details for mail notifier: %v", notifier)
+		HandleHttpError(rw, errors.New("insufficient detail for mail notifier"))
+		return
+	}
 
 	if notifier.MailId == "" || notifier.Passcode == "" || notifier.SmtpServer == "" || notifier.Port == 0 || notifier.Encryption == "" {
-		logger.Get().Error("Insufficient details of add mail notifier: %v", notifier)
+		logger.Get().Error("Insufficient details for add mail notifier: %v", notifier)
 		HandleHttpError(rw, errors.New("insufficient detail for add mail notifier"))
 		return
 	}
@@ -106,5 +125,38 @@ func (a *App) AddMailNotifier(rw http.ResponseWriter, req *http.Request) {
 		logger.Get().Error("Error Updating the mail notifier info for: %s Error: %v", notifier.MailId, err)
 		HandleHttpError(rw, err)
 	}
+	return
+}
+func (a *App) TestMailNotifier(rw http.ResponseWriter, req *http.Request) {
+	var recepient []string
+	vars := mux.Vars(req)
+	recepient = []string{vars["recipient"]}
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		logger.Get().Error("Error parsing http request body:%s", err)
+		HandleHttpError(rw, err)
+		return
+	}
+	var m map[string]interface{}
+
+	if err = json.Unmarshal(body, &m); err != nil {
+		logger.Get().Error("Unable to Unmarshall the data:%s", err)
+		HandleHttpError(rw, err)
+		return
+	}
+	notifier, err := GetDetails(m)
+	if err != nil {
+		logger.Get().Error("Insufficient details for mail notifier: %v", notifier)
+		HandleHttpError(rw, errors.New("insufficient detail for mail notifier"))
+		return
+	}
+
+	if notifier.MailId == "" || notifier.Passcode == "" || notifier.SmtpServer == "" || notifier.Port == 0 || notifier.Encryption == "" {
+		logger.Get().Error("Insufficient details for Test mail notifier: %v", notifier)
+		HandleHttpError(rw, errors.New("insufficient details for Test mail notifier"))
+		return
+	}
+
+	mail_notifier.TestMailNotify(notifier, "Test mail from skyring", "This is a test mail sent from skyring server", recepient)
 	return
 }
