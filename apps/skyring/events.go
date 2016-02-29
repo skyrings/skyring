c060c0c506b909da4412d33dba6759b6a26de83a
nnDarshan <darshan.n.2024@gmail.com>
2016-02-29 15:36:25 +0530

    Event: Enhanced the event listing API.
    This patch adds filtering capability to events api, events
    can be filtered based on severity, ack-state(acked or not),
    node, cluster. And pagination support is also added
    to the api.
    eg: GET request
    /api/v1/events?severity=<severity>&acked=<true/false>&
             node-name=<node name>&cluster-name=<cluster name>
             &page-size=<page size>&page-no=<page no>
    
    This patch also adds a new Api to query the details of a
    single event based on the event id:
    eg: GET request
    /api/vi/event/<event-id>
    
    Change-Id: Iab2298296329face95b305b0b34f1225a707d65d
    Signed-off-by: nnDarshan <darshan.n.2024@gmail.com>

--- apps/skyring/events.go
+++ apps/skyring/events.go
@@ -14,6 +14,8 @@ package skyring
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/skyrings/skyring-common/conf"
 	"github.com/skyrings/skyring-common/db"
 	"github.com/skyrings/skyring-common/models"
@@ -21,44 +23,121 @@ import (
 	"github.com/skyrings/skyring-common/tools/uuid"
 	"gopkg.in/mgo.v2/bson"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func GetEvents(rw http.ResponseWriter, req *http.Request) {
-	sessionCopy := db.GetDatastore().Copy()
-	defer sessionCopy.Close()
-	collection := sessionCopy.DB(conf.SystemConfig.DBConfig.Database).C(models.COLL_NAME_APP_EVENTS)
-	var events []models.AppEvent
+	var filter bson.M = make(map[string]interface{})
 
-	node_id_str := req.URL.Query().Get("node_id")
-	cluster_id_str := req.URL.Query().Get("cluster_id")
-	if len(node_id_str) != 0 {
-		node_id, err := uuid.Parse(node_id_str)
-		if err != nil {
-			logger.Get().Error("Error parsing node id: %s. error: %v", node_id_str, err)
-			HandleHttpError(rw, err)
-			return
-		}
-		err = collection.Find(bson.M{"nodeid": *node_id}).All(&events)
-	} else if len(cluster_id_str) != 0 {
-		cluster_id, err := uuid.Parse(cluster_id_str)
-		if err != nil {
-			logger.Get().Error("Error parsing cluster id: %s. error: %v", cluster_id_str, err)
-			HandleHttpError(rw, err)
-			return
-		}
-		err = collection.Find(bson.M{"clusterid": *cluster_id}).All(&events)
-	} else {
-		err = collection.Find(nil).All(&events)
+	ctxt, err := GetContext(req)
+	if err != nil {
+		logger.Get().Error("Error Getting the context. error: %v", err)
 	}
 
-	if err != nil {
-		logger.Get().Error("Error getting record from DB: %v", err)
+	node_name := req.URL.Query().Get("node-name")
+	cluster_name := req.URL.Query().Get("cluster-name")
+	severity := req.URL.Query().Get("severity")
+	acked := req.URL.Query().Get("acked")
+
+	if len(node_name) != 0 {
+		filter["nodename"] = node_name
+	}
+	if len(cluster_name) != 0 {
+		filter["cluster_name"] = cluster_name
+	}
+
+	var event_severity = map[string]models.AlarmStatus{
+		"indeterminate": models.ALARM_STATUS_INDETERMINATE,
+		"critical":      models.ALARM_STATUS_CRITICAL,
+		"major":         models.ALARM_STATUS_MAJOR,
+		"minor":         models.ALARM_STATUS_MINOR,
+		"warning":       models.ALARM_STATUS_WARNING,
+		"cleared":       models.ALARM_STATUS_CLEARED,
+	}
+
+	if len(severity) != 0 {
+		if s, ok := event_severity[severity]; !ok {
+			logger.Get().Error("%s-Un-supported query param: %v", ctxt, severity)
+			HttpResponse(rw, http.StatusInternalServerError, fmt.Sprintf("Un-supported query param: %s", severity))
+			return
+		} else {
+			filter["severity"] = s
+		}
+	}
+	if len(acked) != 0 {
+		if strings.ToLower(acked) == "true" {
+			filter["acked"] = true
+		} else if strings.ToLower(acked) == "false" {
+			filter["acked"] = false
+		} else {
+			logger.Get().Error("%s-Un-supported query param: %v", ctxt, acked)
+			HttpResponse(rw, http.StatusInternalServerError, fmt.Sprintf("Un-supported query param: %s", acked))
+			return
+		}
+	}
+
+	sessionCopy := db.GetDatastore().Copy()
+	defer sessionCopy.Close()
+	coll := sessionCopy.DB(conf.SystemConfig.DBConfig.Database).C(models.COLL_NAME_APP_EVENTS)
+	var events []models.AppEvent
+	pageNo, pageNoErr := strconv.Atoi(req.URL.Query().Get("page-no"))
+	pageSize, pageSizeErr := strconv.Atoi(req.URL.Query().Get("page-size"))
+	if err := coll.Find(filter).Sort("-timestamp").All(&events); err != nil {
+		logger.Get().Error("%s-Error getting record from DB: %v", ctxt, err)
 		HandleHttpError(rw, err)
 		return
 	}
 	if len(events) == 0 {
 		json.NewEncoder(rw).Encode([]models.AppEvent{})
 	} else {
-		json.NewEncoder(rw).Encode(events)
+		var startIndex int = 0
+		var endIndex int = models.EVENTS_PER_PAGE
+		if pageNoErr == nil && pageSizeErr == nil {
+			startIndex, endIndex = Paginate(pageNo, pageSize, models.EVENTS_PER_PAGE)
+		}
+		if endIndex > len(events) {
+			endIndex = len(events) - 1
+		}
+		json.NewEncoder(rw).Encode(
+			struct {
+				Totalcount int               `json:"totalcount"`
+				Startindex int               `json:"startindex"`
+				Endindex   int               `json:"endindex"`
+				Events     []models.AppEvent `json:"events"`
+			}{len(events), startIndex, endIndex, events[startIndex : endIndex+1]})
+	}
+}
+
+func GetEventById(w http.ResponseWriter, r *http.Request) {
+	ctxt, err := GetContext(r)
+	if err != nil {
+		logger.Get().Error("Error Getting the context. error: %v", err)
+	}
+
+	vars := mux.Vars(r)
+	event_id_str := vars["event-id"]
+	event_id, err := uuid.Parse(event_id_str)
+	if err != nil {
+		logger.Get().Error("%s-Error parsing event id: %s", ctxt, event_id_str)
+		HttpResponse(w, http.StatusBadRequest, fmt.Sprintf("Error parsing event id: %s", event_id_str))
+		return
+	}
+
+	sessionCopy := db.GetDatastore().Copy()
+	defer sessionCopy.Close()
+
+	collection := sessionCopy.DB(conf.SystemConfig.DBConfig.Database).C(models.COLL_NAME_APP_EVENTS)
+	var event models.AppEvent
+	if err := collection.Find(bson.M{"eventid": *event_id}).One(&event); err != nil {
+		logger.Get().Error("%s-Error getting the event detail for %v. error: %v", ctxt, *event_id, err)
+	}
+
+	if event.Name == "" {
+		HttpResponse(w, http.StatusBadRequest, "Event not found")
+		logger.Get().Error("%s-Event: %v not found. error: %v", ctxt, *event_id, err)
+		return
+	} else {
+		json.NewEncoder(w).Encode(event)
 	}
 }
