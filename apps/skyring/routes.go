ea1c29bcb5ae0762e777a33c88c135db14e531f2
nnDarshan <darshan.n.2024@gmail.com>
2016-02-29 15:36:25 +0530

    Event: Enhanced the event listing API.
    This patch adds filtering capability to events api, events
    can be filtered based on severity, ack-state(acked or not),
    node, cluster, text match from event message. And pagination
    support is also added to the api.
    eg: GET request
    /api/v1/events?severity=<severity>&acked=<true/false>&
             node-name=<node name>&cluster-name=<cluster name>
             &page-size=<page size>&page-no=<page no>&
             search-message=<text to be searched in event message>
    
    This patch also adds a new Api to query the details of a
    single event based on the event id:
    eg: GET request
    /api/vi/event/<event-id>
    
    Change-Id: Iab2298296329face95b305b0b34f1225a707d65d
    Signed-off-by: nnDarshan <darshan.n.2024@gmail.com>

--- apps/skyring/routes.go
+++ apps/skyring/routes.go
@@ -408,6 +408,13 @@ func (a *App) LoadRoutes() {
 			HandlerFunc: GetEvents,
 			Version:     1,
 		},
+		{
+			Name:        "GET_event",
+			Method:      "GET",
+			Pattern:     "event/{event-id}",
+			HandlerFunc: GetEventById,
+			Version:     1,
+		},
 		//Block Devices
 		{
 			Name:        "POST_BlockDevices",
