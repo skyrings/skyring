e83b3b9d98cdae9dd76bd7e67766145a2e6629af
Shubhendu Tripathi <shtripat@redhat.com>
2016-03-03 16:00:37 +0530

    skyring: Modified to create cluster sync schedule job
    Earlier only master sync used to happen as part of
    service start. Modified to run cluster details sync
    as a scheduled job.
    
    The sync interval is configurable and can be set
    in /etc/skyring/skyring.conf as below -
    
    --------------------------------------
    "schedule": {
            "clustersSyncInterval: 86400
    }
    --------------------------------------
    
    Default value is set to 24 Hrs (86400 seconds)
    
    Change-Id: I75f5c5f25611489050ed144499308709ac08cf8c
    Signed-off-by: Shubhendu Tripathi <shtripat@redhat.com>

--- apps/skyring/monitoring.go
+++ apps/skyring/monitoring.go
@@ -198,8 +198,7 @@ func (a *App) Get_SystemUtilization(w http.ResponseWriter, r *http.Request) {
 //In memory ClusterId to ScheduleId map
 var ClusterMonitoringSchedules map[uuid.UUID]uuid.UUID
 
-func InitSchedules() {
-	schedule.InitShechuleManager()
+func InitMonitoringSchedules() {
 	if ClusterMonitoringSchedules == nil {
 		ClusterMonitoringSchedules = make(map[uuid.UUID]uuid.UUID)
 	}
