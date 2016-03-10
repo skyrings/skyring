81ff17f9321ecea26abdb61c56fc6fb6ae8d990d
Shubhendu Tripathi <shtripat@redhat.com>
2016-03-11 00:43:57 +0530

    skyring: Added api for import cluster
    Added the below API
      - POST /api/<ver>/importcluster
      request body:
        {
          "bootstrapnode": <node name>,
          "clustertype": <type of cluster>
        }
    
    Change-Id: I199205aeaa690cde14bb9708ae2d464983e0d6c5
    Signed-off-by: Shubhendu Tripathi <shtripat@redhat.com>

--- apps/skyring/routes.go
+++ apps/skyring/routes.go
@@ -374,6 +374,14 @@ func (a *App) LoadRoutes() {
 			Version:     1,
 		},
 		{
+			Name:        "ImportCluster",
+			Method:      "POST",
+			Pattern:     "importcluster",
+			HandlerFunc: a.ImportCluster,
+			Version:     1,
+		},
+		//Routes for Storage management
+		{
 			Name:        "POST_Storages",
 			Method:      "POST",
 			Pattern:     "clusters/{cluster-id}/storages",
