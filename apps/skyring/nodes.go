248aeb25263818678dddbbd899aa255f9f6bde40
anmolbabu <anmolbudugutta@gmail.com>
2016-04-23 01:21:40 +0530

    monitoring: Add utilizations to summary api
    This patch adds cpu, memory, storage and swap
    live/instantaneous values to node summary api.
    It achieves this by periodically syncing all these
    information to the node entity along with a few other
    utilizations that were previously synced.
    
    Change-Id: Ia0a7cedfe578e415acf2d00305e2d86d1b47ec3d
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- apps/skyring/nodes.go
+++ apps/skyring/nodes.go
@@ -610,11 +610,6 @@ func (a *App) GET_NodeSummary(w http.ResponseWriter, r *http.Request) {
 			ctxt,
 			*node_id,
 			err)
-		HttpResponse(
-			w,
-			http.StatusInternalServerError,
-			fmt.Sprintf("Error getting cluster for node. error: %v", err))
-		return
 	}
 
 	uptime, err := GetCoreNodeManager().NodeUptime(node.Hostname, ctxt)
@@ -634,6 +629,7 @@ func (a *App) GET_NodeSummary(w http.ResponseWriter, r *http.Request) {
 		"role":          node.Roles,
 	}
 	nodeSummary[models.COLL_NAME_STORAGE_LOGICAL_UNITS] = getSLUStatusWiseCount(node_id, ctxt)
+	nodeSummary[models.UTILIZATIONS] = node.Utilizations
 	json.NewEncoder(w).Encode(nodeSummary)
 }
 
