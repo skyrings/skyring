62a01179de021ed2cbcfec7bb3b8b0e75d4fdaac
Darshan N <darshan.n.2024@gmail.com>
2016-05-31 16:57:08 +0530

    Url-Redirect: Handles redirection of skyring and graphite web.
    This patch handles redirection of skyring and graphite-web on
    skyring server. The current behaviour is as follows:
    
    1. Skyring service: By default ssl will be enabled for skyring
    service. All the following URLs will redirect to the skyring
    login page (ssl enabled https://<host-name>:10443/#/).
    * https://<hostname>/skyring
    * http://<hostname>/skyring
    * <hostname>/skyring
    
    2. Graphite-web: This service will be now running in port 10080
    and to access this user can use the following URLs:
    * http://<hostname>/graphite-web
    * <hostname>/graphite-web
    
    Note: If just hostname is provided then  neither of these services
    will be displayed, instead the default index.html on a rhel machine
    is displayed.
    
    Change-Id: I89d443fdaa91245a3c444fe1741be78a425b6402
    Signed-off-by: Darshan N <darshan.n.2024@gmail.com>

--- skyring-setup.sh
+++ skyring-setup.sh
@@ -23,10 +23,6 @@ function create_and_install_certificate {
     openssl x509 -req -days 365 -in skyring.csr -signkey skyring.key -out skyring.crt || exit_on_error "Failed to sign the SSL certificate"
     cp skyring.key /etc/pki/tls
     cp skyring.crt /etc/pki/tls
-    cat > /etc/httpd/conf.d/ssl.conf << EOF
-SSLCertificateFile /etc/pki/tls/skyring.crt
-SSLCertificateKeyFile /etc/pki/tls/skyring.key
-EOF
     cd -
     \rm -rf ~/.skyring
 }
@@ -91,15 +87,20 @@ read -p "Do you wish setup to configure that, or prefer to perform that manually
 case $yn in
 	[Yy]* )
 		create_and_install_certificate
-		grep -q "sslEnabled" /etc/skyring/skyring.conf || sed -i -e 's/"config".*{/"config": {\n\t"sslEnabled": true,/g' /etc/skyring/skyring.conf
 		;;
 	[Nn]* )
 		;;
 esac
 
+grep -q "sslEnabled" /etc/skyring/skyring.conf || sed -i -e 's/"config".*{/"config": {\n\t"sslEnabled": true,/g' /etc/skyring/skyring.conf
+
+echo 'Define host_name' `hostname` | cat - /etc/httpd/conf.d/skyring-web.conf > temp && mv -f temp /etc/httpd/conf.d/skyring-web.conf
+
 # Start the skyring server
 systemctl enable skyring
 systemctl start skyring
+
+service httpd restart
 
 info "\n\n\n-------------------------------------------------------"
 info "Now the skyring setup is ready!"
