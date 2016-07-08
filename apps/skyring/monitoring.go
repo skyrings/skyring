90d0fcb9f30f07ecf37532abc966e3fbd473c55e
anmolbabu <anmolbudugutta@gmail.com>
2016-07-08 15:37:40 +0530

    expand_cluster : Correct cluster expansion case monitoring configuration
    Correct cluster expansion case monitoring configuration
    
    Change-Id: Idf44b4b3c5ddac6cda3c30cc51b80bac77eda5e1
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- apps/skyring/monitoring.go
+++ apps/skyring/monitoring.go
@@ -841,9 +841,11 @@ func forceUpdatePlugins(cluster models.Cluster, nodes []string, ctxt string) err
 	nodesWithStaleMonitoringConfig.AddAll(util.GenerifyStringArr(cluster.Monitoring.StaleNodes))
 	nodesWithStaleMonitoringConfig.AddAll(util.GenerifyStringArr(nodes))
 	monState.Plugins = cluster.Monitoring.Plugins
-	plugin_names := make([]string, len(cluster.Monitoring.Plugins))
-	for index, plugin := range cluster.Monitoring.Plugins {
-		plugin_names[index] = plugin.Name
+	var plugin_names []string
+	for _, plugin := range cluster.Monitoring.Plugins {
+		if util.StringInSlice(plugin.Name, monitoring.SupportedMonitoringPlugins) {
+			plugin_names = append(plugin_names, plugin.Name)
+		}
 	}
 	if forUpdateErrors, forceUpdatePythonError := GetCoreNodeManager().EnforceMonitoring(
 		plugin_names,
