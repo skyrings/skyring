d2ba9274e13e57d52b65caf6dde228413e578ba2
Shubhendu <shtripat@redhat.com>
2016-08-06 00:12:38 +0530

    Revert "rpm: generate unique password for admin during installation"
    This reverts commit 3d76e9cebbc20a395d6d6468ce015011061065c6.
    
    Change-Id: I67c425225f22867995433b286b26eed150a8140f

--- skyring-pre.sh
+++ skyring-pre.sh
@@ -3,8 +3,6 @@ USER=`/bin/mongo -quiet skyring --eval 'db.getUser("admin")["user"]'`
 if [ $? -ne 0 ] || [ "${USER}" != 'admin' ]; then
     /bin/systemctl status mongod > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-	pwd=`python -c 'import json;print json.loads(open("/etc/skyring/skyring.conf").read())["dbconfig"]["password"]'`
-	cmd="/bin/mongo skyring --eval 'db.createUser( { \"user\" : \"admin\", \"pwd\": \"$pwd\", \"roles\" : [\"readWrite\", \"dbAdmin\", \"userAdmin\"] })'"
-	eval $cmd
+	/bin/mongo skyring --eval 'db.createUser( { "user" : "admin", "pwd": "admin", "roles" : ["readWrite", "dbAdmin", "userAdmin"] })'
     fi
 fi
