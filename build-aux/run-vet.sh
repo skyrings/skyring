1fe6d3df272e6249e77c0192b1e79720676edef4
Martin Bukatovic <mbukatov@redhat.com>
2016-08-22 13:20:09 +0200

    replace bundled vet binary with `go tool vet`
    Since commit 12a7ff4621a22247dcca1193ee12618df7f78cd9, `build-aux` directory
    contains a vet binary file with some misterious version of the go vet
    tool. There is no explanation of why this binary file was included
    either in the commit itself or in related gerrithub change[1].
    
    This commit fixes the problem by using standard vet command as provided
    by golang compiler itself[2].
    
    [1] https://review.gerrithub.io/#/c/249790/
    [2] https://golang.org/cmd/vet/
    
    Change-Id: I8f04e2a30d797a0e4354594a8cfd09826fda9b92
    Signed-off-by: Martin Bukatovic <mbukatov@redhat.com>

--- build-aux/run-vet.sh
+++ build-aux/run-vet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-${PWD}/build-aux/vet ./main.go
+go tool vet ./main.go
 for ENTRY in `ls . | grep -v '^vendor'`
 do
 	if [ -d ${ENTRY} ]
 	then
-		${PWD}/build-aux/vet $ENTRY
+		go tool vet $ENTRY
 	fi
 done
