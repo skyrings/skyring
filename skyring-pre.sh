45a6c19f5aed03fdc47a4b0ee6ef2c525ae8b120
Timothy Asir <tjeyasin@redhat.com>
2016-09-27 20:12:51 +0530

    skyring: Start mongodb in auth mode with random password
    This patch does the following things
    1) On Skyring-setup / install
       By-default, the security flag will be disabled and the
         db might not have any credentials
       This script will add the credentials and return
    2) On reguler service restart
       It will check for the security flag and validate the credentials
       If the flage is enabled and the authentication is unsuccess
         it will update the password as per configuration and return.
       Return peacfully, If the security flag is enabled
         and the authentication is successful.
    3) On update
       It will use the existing credential and continues
    4) On Skyring-setup rerun
       The configuration might have received a new password
       This script will reset the password and return
    
    Change-Id: Ie5091daf2600e361034655afb02558316bbf2613
    Signed-off-by: Timothy Asir <tjeyasin@redhat.com>

--- skyring-pre.sh
+++ skyring-pre.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
-USER=`/bin/mongo -quiet skyring --eval 'db.getUser("admin")["user"]'`
-if [ $? -ne 0 ] || [ "${USER}" != 'admin' ]; then
-    /bin/systemctl status mongod > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
-	/bin/mongo skyring --eval 'db.createUser( { "user" : "admin", "pwd": "admin", "roles" : ["readWrite", "dbAdmin", "userAdmin"] })'
+pwd=`python -c 'import json;print json.loads(open("/etc/skyring/skyring.conf").read())["dbconfig"]["password"]'`
+# Check the db status
+/bin/systemctl status mongod > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    # Check the db connection and fetch the user name
+    UNAME=`/bin/mongo -quiet skyring --eval 'db.getUser("admin")["user"]' -u admin -p ${pwd}`
+    if [ $? -ne 0 ]; then
+	# Reset the auth flag to false because the password is changed
+	/bin/grep "^auth = true" /etc/mongodb.conf > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+	    sed -i -e 's/auth = true/#auth = true/g' /etc/mongodb.conf
+	    /bin/systemctl restart mongod > /dev/null 2>&1
+	fi
+	/bin/systemctl status mongod > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+	    # Check admin user exists already
+	    USERNAME=`/bin/mongo -quiet skyring --eval 'db.getUser("admin")["user"]'`
+	    if [ $? -ne 0 ] || [ "${USERNAME}" != 'admin' ]; then
+		# Admin user does not exists
+		cmd="/bin/mongo skyring --eval 'db.createUser( { \"user\" : \"admin\", \"pwd\": \"$pwd\", \"roles\" : [\"readWrite\", \"dbAdmin\", \"userAdmin\"] })'"
+	    else
+		# Admin user already exists. Update the password
+		cmd="/bin/mongo skyring --eval 'db.changeUserPassword(\"admin\", \"${pwd}\")'"
+	    fi
+	    eval $cmd > /dev/null 2>&1 || :
+	    # Update the security flag and restart the mongod service
+	    /bin/grep "^#auth = true" /etc/mongodb.conf > /dev/null 2>&1
+	    if [ $? -eq 0 ]; then
+		sed -i -e 's/#auth = true/auth = true/g' /etc/mongodb.conf
+	    fi
+	    /bin/systemctl restart mongod > /dev/null 2>&1
+	fi
     fi
 fi
